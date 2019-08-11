bef31abe55b1ecf8468d4e0f76beefbc10cc1605
James Swineson <github@public.swineson.me>
2019-08-11 09:40:26 +0800

    fix docker scripts according to code changes

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 ! update-remote-config.sh
 
 if [[ "$1" == supervisord ]] || [ "$1" == dnsdist ]; then
-    dnsdist-autoconf -config /etc/dnsdist/autoconf.toml -output /etc/dnsdist/dnsdist.conf -docker
+    dnsdist-autoconf -config /etc/dnsdist -docker
 fi
 
 exec "$@"
--- docker/reload.sh
+++ docker/reload.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 ! update-remote-config.sh
 
-dnsdist-autoconf -config /etc/dnsdist/autoconf.toml -output /etc/dnsdist/dnsdist.conf -docker
+dnsdist-autoconf -config /etc/dnsdist -docker
 
 if dnsdist --check-config; then
     supervisorctl restart dnsdist
